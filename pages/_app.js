6400962c55bc149d97a79403beca64b4b67a700a
Glenn Rempe <glenn@truestamp.com>
2021-10-03 11:12:38 -0400

    Add favicon headers and assets and other meta info

--- pages/_app.js
+++ pages/_app.js
@@ -65,6 +65,30 @@ export default function App({ }) {
     <body className="flex flex-col min-h-screen bg-white dark:bg-gray-800">
       <Head>
         <title>Truestamp | Observable Entropy</title>
+
+        <meta name="description" content="Truestamp Observable Entropy captures and store public sources of entropy and generates incontestable public randomness that is independently verifiable." />
+        <meta name="robots" content="index,follow" />
+        <meta name="author" content="Truestamp" />
+
+        {/* http://humanstxt.org */}
+        <link type="text/plain" rel="author" href="/humans.txt" />
+
+        {/*
+        Favicon
+        https://realfavicongenerator.net/
+        https://realfavicongenerator.net/favicon_result?file_id=p1eg3vq3c81jko89la8cbpn1b3d6
+        Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
+        */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" crossOrigin="use-credentials" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1059a9" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Truestamp" />
+        <meta name="application-name" content="Truestamp" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main className="flex-auto h-40">
