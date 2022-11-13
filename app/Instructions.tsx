79534a91953dcf7a7de7f63ce0920966edfb8ab2
Glenn Rempe <glenn@truestamp.com>
2022-11-13 17:23:52 -0500

    Enhance instructions.

--- app/Instructions.tsx
+++ app/Instructions.tsx
@@ -7,7 +7,8 @@ export default function Instructions() {
         Print or display this QR code so that it is clearly visible in the
         background of a photo or video. It will auto-refresh about every 5
         minutes. Anyone scanning the QR code will be able to verify your media
-        was created after a point in time.{" "}
+        was created after a point in time. If printing, refresh the print once
+        per day.{" "}
         <Link
           href="https://github.com/truestamp/observable-entropy-v2/blob/main/README.md"
           target="_blank"
